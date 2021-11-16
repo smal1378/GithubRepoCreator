de1bf3c5ecf6b031783b891856b4a95c033fec48
Esmail <smal137800@gmail.com>
2021-11-17 02:19:44 +0330

    add type hint to model.py and fix a bug in TokenManagerKeyring.load_token class.

--- model.py
+++ model.py
@@ -2,13 +2,16 @@ from github import Github
 from github.GithubException import UnknownObjectException
 from keyring import set_password, get_password
 from keyring.errors import InitError
+from typing import List, Optional, Tuple, Callable
 
 
 class LogConsole:
     def __init__(self, max_size: int = 1000):
-        self.lst = [None] * max_size  # this list can be optimized using a Queue implemented with a linked list
+        # this list can be optimized using a Queue implemented with a linked list
+        # although it is currently fast enough for usages bellow 1000 amount of log.
+        self.lst: List[Optional[Tuple[str, str]]] = [None] * max_size
         self.pointer = 0
-        self.callbacks = []
+        self.callbacks: List[Callable[[Tuple[str, str]], None]] = []
 
     def get_log(self, count: int = 100):
         x = 0
@@ -26,7 +29,7 @@ class LogConsole:
         for i in self.callbacks:
             i((module, message))
 
-    def attach_callback(self, callback) -> int:
+    def attach_callback(self, callback: Callable[[Tuple[str, str]], None]) -> int:
         """
         Will call the callback whenever a new log message has been added.
         :param callback: callback(Tuple[str, str]), Tuple contains module name and message text
@@ -111,21 +114,23 @@ class TokenManagerKeyring(TokenManagerMother):
     keyring_service_name = "PythonRepoCreator"
 
     def __init__(self, logger: LogConsole):
-        self.token = None
+        self.token: Optional[str] = None
         self.logger = logger
         self.load_token()
 
-    def load_token(self, name: str = "Default"):
-        try:
-            self.token = get_password(self.keyring_service_name, "Token")
-        except InitError as err:
-            self.logger.log("TokenManager", f"Access to keyring denied."
-                                            f"\n{repr(err)}")
+    def load_token(self, name: str = "Default") -> str:
+        if not self.token:
+            try:
+                self.token = get_password(self.keyring_service_name, "Token")
+            except InitError as err:
+                self.logger.log("TokenManager", f"Access to keyring denied."
+                                                f"\n{repr(err)}")
+        return self.token
 
     def set_token(self, token: str, name: str = "Default"):
         self.token = token
         set_password(self.keyring_service_name, "Token", token)
         self.logger.log("TokenManager", "New token has been set.")
 
-    def has_token(self, name: str = "Default"):
+    def has_token(self, name: str = "Default") -> bool:
         return bool(self.token)
